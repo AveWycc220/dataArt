8e030543df497b458aabe804faff04c06682d08d
AveWycc220 <mik16748@gmail.com>
2021-04-17 12:02:08 +0300

    feat (tasks): last task init

--- worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('message', e => {
-  postMessage(e.data.arr
-    .map(elem => elem.email.toLowerCase().includes(e.data.value.toLowerCase()) ? elem : null)
-    .filter(elem => Boolean(elem)))
-})
